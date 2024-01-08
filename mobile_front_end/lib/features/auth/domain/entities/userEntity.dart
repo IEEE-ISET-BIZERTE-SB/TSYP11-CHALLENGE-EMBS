8322ae9450b6d5cd3a522297ede6f426dc33b8b3
hkimi amin <hkimiamin02@gmail.com>
2024-01-09 00:47:39 +0100

    update user entity

--- mobile_front_end/lib/features/auth/domain/entities/userEntity.dart
+++ mobile_front_end/lib/features/auth/domain/entities/userEntity.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 class UserEntity extends Equatable {
   final String? id;
   final String username;
-  final String? password;
+  String? password;
   final List<String>? roles;
   final String? accessToken;
   UserEntity(
