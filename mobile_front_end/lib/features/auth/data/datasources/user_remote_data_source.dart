23d2b88d3dbc12db7fa8d43d633d85539b6db166
hkimi amin <hkimiamin02@gmail.com>
2024-01-09 00:45:55 +0100

    remote data sources

--- mobile_front_end/lib/features/auth/data/datasources/user_remote_data_source.dart
+++ mobile_front_end/lib/features/auth/data/datasources/user_remote_data_source.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:http/http.dart' as http;
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobile_front_end/core/exceptions/exceptions.dart';
 import 'package:mobile_front_end/features/auth/data/models/userModel.dart';
+import 'package:http/http.dart' as http;
 
 abstract class UserRemoteDataSource {
   Future<UserModel> signInUser(UserModel userModel);
@@ -12,38 +10,34 @@ abstract class UserRemoteDataSource {
 
 class UserRemoteDataSourceImpl extends UserRemoteDataSource {
   final http.Client client;
-  UserRemoteDataSourceImpl({required this.client});
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  UserRemoteDataSourceImpl({http.Client? client})
+      : client = client ?? http.Client();
   @override
   Future<UserModel> signInUser(UserModel userModel) async {
-    final body = jsonEncode(
-        {"username": userModel.username, "password": userModel.password});
-    late final response;
     try {
-      response = await client.post(
-          Uri.parse("http://192.168.100.114:8081/api" + "/auth/signin/"),
-          headers: {
-            "content-type": "application/json",
-            "accept": "application/json",
-          },
-          body: body);
-    } catch (e) {
-      print("exception " + e.toString());
-    }
-    if (response.statusCode == 200) {
-      try {
-        final user = UserModel.fromJson(jsonDecode(response.body));
-        return Future.value(user);
-      } catch (e) {
-        return Future.value(null);
-      }
-    } else {
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: userModel.username,
+        password: userModel.password ?? '',
+      );
+      UserModel user = UserModel(
+        id: userCredential.user!.uid,
+        username: userCredential.user!.email ?? '',
+      );
+
+      return Future.value(user);
+    } on FirebaseAuthException catch (e) {
+      print("Firebase Auth Exception: ${e.message}");
       throw LoginException();
+    } catch (e) {
+      print("Exception: $e");
+      return Future.value(null);
     }
   }
 
   @override
-  Future<void> signOutUser() {
-// TODO: implement signOutUser
-    throw UnimplementedError();
+  Future<void> signOutUser() async {
+    await _firebaseAuth.signOut();
   }
 }
