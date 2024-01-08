3f76af4d18a7c09d57d5c841e9674e0ad1308d2b
hkimi amin <hkimiamin02@gmail.com>
2024-01-09 00:46:42 +0100

    update model

--- mobile_front_end/lib/features/auth/data/models/userModel.dart
+++ mobile_front_end/lib/features/auth/data/models/userModel.dart
@@ -3,25 +3,26 @@ import 'dart:convert';
 import 'package:mobile_front_end/features/auth/domain/entities/userEntity.dart';
 
 class UserModel extends UserEntity {
-  UserModel(
-      {String? id,
-      required String username,
-      password,
-      List<String>? roles,
-      String? accessToken})
-      : super(
+  UserModel({
+    String? id,
+    required String username,
+    List<String>? roles,
+    String? accessToken,
+    String? password,
+  }) : super(
             id: id,
             username: username,
-            password: password,
             roles: roles,
-            accessToken: accessToken);
-  factory UserModel.fromJson(Map<String, dynamic> json) {
+            accessToken: accessToken) {
+    this.password = password ?? '';
+  }
+  factory UserModel.fromFirebase(Map<String, dynamic> json) {
     return UserModel(
-        id: '1', //json['id'].toString(),
-        username: json['username'],
-        password: json['password'],
-        roles: List.from(json['roles']), //.cast<String>(),
-        accessToken: json['accessToken']);
+      id: json['uid'], // Firebase UID can be used as the ID
+      username: json['email'],
+      roles: <String>[], // Firebase doesn't provide roles directly
+      accessToken: null, // Firebase uses tokens for authentication
+    );
   }
   Map<String, dynamic> toJson() {
     return {
@@ -31,5 +32,14 @@ class UserModel extends UserEntity {
       'roles': jsonEncode(roles),
       'accessToken': accessToken
     };
+  }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+        id: '1', //json['id'].toString(),
+        username: json['username'],
+        password: json['password'],
+        roles: List.from(json['roles']), //.cast<String>(),
+        accessToken: json['accessToken']);
   }
 }
