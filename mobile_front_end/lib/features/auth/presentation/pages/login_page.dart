4a460a6e99fe0c37ac9df49a15b1e5790e1c686f
hkimi amin <hkimiamin02@gmail.com>
2024-01-09 00:48:27 +0100

    update login page

--- mobile_front_end/lib/features/auth/presentation/pages/login_page.dart
+++ mobile_front_end/lib/features/auth/presentation/pages/login_page.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_front_end/core/screens/home_page.dart';
+import 'package:mobile_front_end/core/screens/starter_widget.dart';
 import 'package:mobile_front_end/core/util/snack_bar_message.dart';
 import 'package:mobile_front_end/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:mobile_front_end/features/auth/presentation/bloc/auth_state.dart';
 import 'package:mobile_front_end/features/auth/presentation/widgets/login_form.dart';
 
-//import 'package:go_router/go_router.dart';
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthBloc, AuthState>(
       listener: (context, state) {
         if (state is AuthenticatedState) {
-//naviguer vers liste de post
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => StarterWidget(),
+            ),
+          );
         } else if (state is AuthErrorState) {
           SnackBarMessage()
               .showErrorSnackBar(message: state.message, context: context);
         }
       },
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          iconTheme:
-              IconThemeData(color: Colors.black), // Icône de retour en noir
-        ),
         backgroundColor: Colors.white,
-        body: LoginForm(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                'assets/images/logo.png', // Replace with the path to your logo image
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(height: 16),
+              // Login Form
+              LoginForm(),
+            ],
+          ),
+        ),
       ),
     );
   }
